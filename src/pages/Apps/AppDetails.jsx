d4c88d5b90c7b3e5bc8009293b79dbdf5ea76e69
tahrim sakib <you@example.comdmsakib1122@gmail.com>
2025-10-08 15:17:17 +0600

    app details and fixed bugs

--- src/pages/Apps/AppDetails.jsx
+++ src/pages/Apps/AppDetails.jsx
@@ -22,50 +22,52 @@ const AppDetails = () => {
   console.log(data);
 
   return (
-    <div className="max-w-11/12 mx-auto my-10">
-      <div className="flex items-center ">
-        <figure className="mr-7">
-          <img className="w-80 object-contain" src={image} alt={title} />
-        </figure>
-        <div>
+    <div className="bg-[#f5f5f5] min-h-screen">
+      <div className="max-w-11/12 mx-auto py-10 ">
+        <div className="flex-col md:flex md:flex-row items-center ">
+          <figure className="mr-7 mb-4 flex justify-center items-center">
+            <img className="w-80 object-contain" src={image} alt={title} />
+          </figure>
           <div>
-            <h1 className="font-bold text-2xl">{title}</h1>
-            <p className="text-gray-500">
-              Company Name:{" "}
-              <span className="text-violet-600 font-semibold">
-                {companyName}
-              </span>
-            </p>
+            <div>
+              <h1 className="font-bold text-2xl md:text-4xl">{title}</h1>
+              <p className="text-gray-500">
+                Company Name:{" "}
+                <span className="text-violet-600 font-semibold">
+                  {companyName}
+                </span>
+              </p>
+            </div>
+            <div className="divider"></div>
+            <div className="flex justify-baseline gap-9 ">
+              <div>
+                <Download></Download>
+                <p className="text-gray-500">Download</p>
+                <h1 className="font-bold text-4xl">{downloads} </h1>
+              </div>
+              <div>
+                <Star></Star>
+                <p className="text-gray-500">ratingAvg</p>
+                <h1 className="font-bold text-4xl">{ratingAvg} </h1>
+              </div>
+              <div>
+                <MessageSquareHeart></MessageSquareHeart>
+                <p className="text-gray-500">Total Reviews</p>
+                <h1 className="font-bold text-4xl">{reviews} </h1>
+              </div>
+            </div>
+            <button className="mt-5 btn btn-primary">
+              Install Now ({size} MB){" "}
+            </button>
           </div>
-          <div className="divider"></div>
-          <div className="flex justify-baseline gap-9 ">
-            <div>
-              <Download></Download>
-              <p className="text-gray-500">Download</p>
-              <h1 className="font-bold text-4xl">{downloads} </h1>
-            </div>
-            <div>
-              <Star></Star>
-              <p className="text-gray-500">ratingAvg</p>
-              <h1 className="font-bold text-4xl">{ratingAvg} </h1>
-            </div>
-            <div>
-              <MessageSquareHeart></MessageSquareHeart>
-              <p className="text-gray-500">Total Reviews</p>
-              <h1 className="font-bold text-4xl">{reviews} </h1>
-            </div>
-          </div>
-          <button className="mt-5 btn btn-primary">
-            Install Now ({size} MB){" "}
-          </button>
         </div>
-      </div>
-      <div className="divider"></div>
-      <div className="">flow chart</div>
-      <div className="divider"></div>
-      <div className="">
-        <h1 className="text-2xl font-semibold">Description</h1>
-        <p className="text-gray-500">{description}</p>
+        <div className="divider"></div>
+        <div className="">flow chart</div>
+        <div className="divider"></div>
+        <div className="">
+          <h1 className="text-2xl font-semibold">Description</h1>
+          <p className="text-gray-500">{description}</p>
+        </div>
       </div>
     </div>
   );
