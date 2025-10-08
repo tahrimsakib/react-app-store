e2f0d0fe9a114105e5110593a078f23a56ecd4fa
tahrim sakib <you@example.comdmsakib1122@gmail.com>
2025-10-08 12:06:35 +0600

    created appdetails file and app card

--- src/pages/Apps/AppDetails.jsx
+++ src/pages/Apps/AppDetails.jsx
@@ -1,3 +1,4 @@
+import { Download, MessageSquareHeart, Star } from "lucide-react";
 import React from "react";
 import { useLoaderData, useParams } from "react-router";
 
@@ -7,13 +8,50 @@ const AppDetails = () => {
   console.log(id);
   const data = useLoaderData();
   const singleData = data.find((app) => app.id === appId);
-  const { title } = singleData;
+  const { title, image, companyName, downloads, ratingAvg, reviews, size } =
+    singleData;
 
   console.log(data);
 
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="max-w-11/12 mx-auto">
+      <div className="flex items-center  ">
+        <figure className="mr-7">
+          <img className="w-80 object-contain" src={image} alt={title} />
+        </figure>
+        <div>
+          <div>
+            <h1 className="font-bold text-2xl">{title}</h1>
+            <p className="text-gray-500">
+              company Name:{" "}
+              <span className="text-violet-600 font-semibold">
+                {companyName}
+              </span>
+            </p>
+            <span className="divider"></span>
+          </div>
+          <div className="flex justify-baseline gap-9 ">
+            <div>
+              <Download></Download>
+              <p className="text-gray-500">Download</p>
+              <h1 className="font-bold text-4xl">{downloads} </h1>
+            </div>
+            <div>
+              <Star></Star>
+              <p className="text-gray-500">ratingAvg</p>
+              <h1 className="font-bold text-4xl">{ratingAvg} </h1>
+            </div>
+            <div>
+              <MessageSquareHeart></MessageSquareHeart>
+              <p className="text-gray-500">Total Reviews</p>
+              <h1 className="font-bold text-4xl">{reviews} </h1>
+            </div>
+          </div>
+          <button className="mt-5 btn btn-primary">
+            Install Now ({size} MB){" "}
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
